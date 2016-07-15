ee011ab244870c6a074780131f34e261fbc023a3
Ilya Maklakaov <ilyamaklakov@gmail.com>
2016-07-15 21:25:29 +0300

    SQL query for second task and cover letter added

---
+++ ВтороеЗадание.sql
@@ -0,0 +1,10 @@
+﻿SELECT Product_Id, COUNT(Product_Id) AS CountOfSalesAsFirst
+FROM [testSconome3].[dbo].[Sales] P
+INNER JOIN
+(SELECT Customer_Id,
+MIN(Date) As FirstDate
+  FROM [dbo].[Sales]
+  GROUP BY Customer_Id) X
+  ON P.Customer_Id = X.Customer_Id AND P.Date = X.FirstDate
+  GROUP BY Product_Id
+  ORDER BY CountOfSalesAsFirst DESC
